f8825f089ea2266fc2ed5a2ae27d6efdc77fdbb4
ajadprajapati224155-prog <ajadprajapati224155@gmail.com>
2026-05-11 15:38:47 +0530

    Update config.py with hardcoded credentials
    Replaced environment variables with hardcoded values for BOT_TOKEN, API_ID, API_HASH, and VIP_USER.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    VIP_USER = os.environ.get('VIP_USERS', '').split(',')
+    BOT_TOKEN = os.environ.get("8669372993:AAEl4wFWPefNAXTH2qmRFRVCOs_LkLbjvwI")
+    API_ID = int(os.environ.get("35279304"))
+    API_HASH = os.environ.get("49ea7646f4251b3ca5a7798c61bb5e9f")
+    VIP_USER = os.environ.get('VIP_USERS', '6824252172').split(',')
     VIP_USERS = [int(user_id) for user_id in VIP_USER]
